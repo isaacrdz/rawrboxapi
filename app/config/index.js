d70cfab9c6414e299b212bbcf359a3745f9a2a5d
isaacrdz <zack_cpp@hotmail.com>
2018-04-10 16:25:53 -0500

    Tes

--- app/config/index.js
+++
@@ -1,9 +0,0 @@
-import mongodb from 'mongodb';
-
-export default {
-  // "port": 3005,
-  // "mongoUrl": "mongodb://localhost:27017/chat-api",
-  "port": process.env.PORT,
-  "mongoUrl": process.env.MONGODB_URI,
-  "bodyLimit": "100kb"
-}
--- app/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-import config from './config';
-
-export default callback => {
-  // mongoose.Promise = Global.promise;
-  let db = mongoose.connect(config.mongoUrl);
-  callback(db);
-}
